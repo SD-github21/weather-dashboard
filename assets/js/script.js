df5b3c46e79a5cf0f79624b111cfa8a0ea5a361f
SD-github21 <sapandonde.gmail.com>
2022-04-22 20:35:05 -0500

    add moment.js current date and dates for 5 day forecast + api info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,10 @@
 // Retrieve Open Weather API call for city name
-// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+// https://api.openweathermap.org/data/2.5/weather?q={city name}cnt=5&units=imperial&appid=
+
+document.getElementById("currentDay").innerHTML = moment().format("MM/DD/YYYY");
+document.getElementById("day1").innerHTML = moment().add(1, 'days').format("MM/DD/YYYY");
+document.getElementById("day2").innerHTML = moment().add(2, 'days').format("MM/DD/YYYY");
+document.getElementById("day3").innerHTML = moment().add(3, 'days').format("MM/DD/YYYY");
+document.getElementById("day4").innerHTML = moment().add(4, 'days').format("MM/DD/YYYY");
+document.getElementById("day5").innerHTML = moment().add(5, 'days').format("MM/DD/YYYY");
+
